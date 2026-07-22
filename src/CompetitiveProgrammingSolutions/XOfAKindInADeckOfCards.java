4f3f59eef762b79370f013899314c6b667cee83a
Antonio Roldán <antonioroldan95@outlook.com>
2026-07-22 20:59:05 +0200

    76 out 78 tests passed

--- src/CompetitiveProgrammingSolutions/XOfAKindInADeckOfCards.java
+++ src/CompetitiveProgrammingSolutions/XOfAKindInADeckOfCards.java
@@ -1,21 +1,30 @@
 package CompetitiveProgrammingSolutions;
 
-import java.sql.Array;
 import java.util.*;
+
 
 class XOfAKindInADeckOfCards {
     public boolean hasGroupsSizeX(int[] deck) {
         List<Integer> deckList = new ArrayList<Integer>();
+        List<Integer> deckSizes = new ArrayList<Integer>();
         Set<Integer> uniqueNumbersInDeckSet = new HashSet<Integer>();
         List<Integer> uniqueNumbersInDeckList = new ArrayList<Integer>();
         uniqueNumbersInDeckList.addAll(uniqueNumbersInDeckSet);
         Map<Integer, ArrayList<Integer>> mapOfNumberToCardsWithThatNumber = new HashMap<Integer, ArrayList<Integer>>();
         int subDeckSize = 0;
         int currentSubDeckSize = 0;
+        int smallestDeckSize = 0;
+        if(deck.length == 1){
+            return false;
+        }
+        for(int i = 0; i < deck.length; i++){
+            deckList.add(deck[i]);
+        }
         for(int i = 0; i < deckList.size(); i++){
             uniqueNumbersInDeckSet.add(deckList.get(i));
         }
         uniqueNumbersInDeckList = new ArrayList<Integer>(uniqueNumbersInDeckSet);
+        System.out.println(uniqueNumbersInDeckList);
         for(int i = 0; i < uniqueNumbersInDeckList.size(); i++){
             mapOfNumberToCardsWithThatNumber.put(uniqueNumbersInDeckList.get(i), new ArrayList<Integer>());
         }
@@ -24,14 +33,19 @@ class XOfAKindInADeckOfCards {
             temp.add(deckList.get(i));
             mapOfNumberToCardsWithThatNumber.put(Integer.valueOf(deck[i]), temp);
         }
-        for(int i = 0; i < uniqueNumbersInDeckSet.size(); i++){
+        for(int i = 0; i < uniqueNumbersInDeckList.size(); i++){
+            deckSizes.add(mapOfNumberToCardsWithThatNumber.get(uniqueNumbersInDeckList.get(i)).size());
+        }
+        smallestDeckSize = Collections.min(deckSizes);
+        for(int i = 0; i < uniqueNumbersInDeckList.size(); i++){
             currentSubDeckSize = mapOfNumberToCardsWithThatNumber.get(uniqueNumbersInDeckList.get(i)).size();
-            if(i == 0){
-                subDeckSize = currentSubDeckSize;
-            } else {
-                if(subDeckSize != currentSubDeckSize){
+            if(currentSubDeckSize % smallestDeckSize == 0 || currentSubDeckSize % 2 == 0 && smallestDeckSize % 2 == 0){
+                if(smallestDeckSize == 1 && currentSubDeckSize > 1){
                     return false;
                 }
+                continue;
+            } else {
+                return false;
             }
         }
         return true;
